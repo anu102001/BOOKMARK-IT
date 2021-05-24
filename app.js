e39d028b41b017f8853051f20ee338d33b66135a
soumya gupta <samygutu.9995@gmail.com>
2021-05-24 15:42:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const request = require("request");
+const express =require("express");
+const app = express();
+const port=3000;
+app.set('views','./views')
+app.set('view engine','ejs');
+app.use(express.static('public'))
+app.use('/css',express.static(__dirname +'public/css'))
+app.use('/css',express.static(__dirname +'public/js'))
+app.use('/css',express.static(__dirname +'public/img'))
+app.get('/',(req, res)=>{
+    res.render('home')
+   
+  
+
+});
+
+app.listen(port,()=>{
+    console.log(`weather app is listening at https://localhost:${port}`)
+});
+
+app.set
